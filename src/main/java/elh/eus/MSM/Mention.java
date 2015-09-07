452124a6e23941d0ac95f1cb27759895283456ec
isanvicente <i.sanvicente@elhuyar.com>
2015-09-07 16:29:06 +0200

    Mention class added, empty class for the moment

---
+++ src/main/java/elh/eus/MSM/Mention.java
@@ -0,0 +1,35 @@
+package elh.eus.MSM;
+
+import java.util.List;
+
+import twitter4j.JSONObject;
+
+public class Mention {
+
+	private int id;
+	private int author_id;
+	private String text;
+	private String url;
+	private List<String> keywords;
+	private String lang;
+	private String polarity;
+	
+	
+	public Mention(JSONObject json) {
+		
+	}
+
+	public Mention() {
+		
+	}
+
+	private void mentionFromTweet() {
+		
+	}
+	
+	private void mentionFromFeed() {
+		
+	}
+	
+
+}
