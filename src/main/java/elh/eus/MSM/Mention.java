e6d67ab72b2814a517fe2461142b3856d4e909c8
isanvicente <i.sanvicente@elhuyar.com>
2016-01-20 10:55:58 +0100

    fixed feed publication date

--- src/main/java/elh/eus/MSM/Mention.java
+++ src/main/java/elh/eus/MSM/Mention.java
@@ -222,7 +222,7 @@ public class Mention {
 			if (getKeywords()!=null && !getKeywords().isEmpty())
 			{
 				for (Keyword k: getKeywords()){
-					System.out.println("Mention2db: text: "+k.getText()+" id: "+k.getScreenTag());
+					System.out.println("Mention2db: text: "+k.getText()+" tag: "+k.getScreenTag()+" lang: "+k.getLang());
 					stmtKM.setInt(1, k.getId());
 					stmtKM.setInt(2, getMention_id());
 					stmtKM.executeUpdate();
