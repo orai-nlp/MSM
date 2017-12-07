c3488814cefde633d290c376f6e9ab04af5c433d
isanvicente <i.sanvicente@elhuyar.eus>
2017-12-07 16:19:00 +0100

    Updated MSM version to 1.3. updated database schema creation script to current version

--- MSM-twitterCrawler.sh
+++ MSM-twitterCrawler.sh
@@ -15,21 +15,21 @@ pid=`pgrep -f "twitter .*MSM-$1.cfg"`
 if [ -z "$pid" ]
 then 
     echo "MSM-twitterCrawler.sh : $1 instantziaren crawlerra erorita, berrabiarazi -- $data --"
-    java -jar $DIR/MSM-1.2.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &
+    java -jar $DIR/MSM-1.3.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &
 else
     noconn=`tail -3 $DIR/log/MSM-twitter.log | grep "Shutting down httpclient connection manager" | wc -l`
     if [ $noconn -gt 0 ]
 	then
 	    echo "MSM-twitterCrawler.sh : $1 twitter crawlerra konexio gabe, berrabiarazi -- $data --"
 	    kill -9 $pid
-	    java -jar $DIR/MSM-1.2.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &
+	    java -jar $DIR/MSM-1.3.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &
     else
 	noconn=`tail -10 $DIR/log/MSM-twitter.log | grep "status code - 420" | wc -l`
 	if  [ $noconn -gt 0 ]
 	then
 	    echo "MSM-twitterCrawler.sh : $1 twitter crawlerra blokeatuta(420), berrabiarazi -- $data --"
 	    kill -9 $pid
-	    java -jar $DIR/MSM-1.2.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &	
+	    java -jar $DIR/MSM-1.3.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &	
 	#else
 	#    echo "MSM-twitterCrawler.sh : $1 twitter crawlerra ongi dagoela dirudi. -- $data --"
 	fi
--- MSM-twtCrawler-restart.sh
+++ MSM-twtCrawler-restart.sh
@@ -13,7 +13,7 @@ data=`date +"%Y%m%d%H%M"`;
 pid=`pgrep -f " twitter .*MSM-$1.cfg"`
 echo "MSM-twtCrawler-restart.sh : $1 twitter crawlerra ITZALTZEN ... -- $data --"
 kill -9 $pid
-java -jar $DIR/MSM-1.2.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &
+java -jar $DIR/MSM-1.3.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &
 echo "MSM-twtCrawler-restart.sh : $1 twitter crawlerra BERRABIARAZITA ... -- $data --"
 
 
