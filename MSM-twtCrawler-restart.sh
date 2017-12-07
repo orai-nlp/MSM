1edb2446b0ed9381d85b9698093a38a43dca791c
isanvicente <i.sanvicente@elhuyar.com>
2017-12-07 11:42:53 +0100

    added twitter crawler restarting script, fixed twitter crawler calling script

---
+++ MSM-twtCrawler-restart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 1 - talaia config file name without .cfg extension (no path) and without 'MSM-' prefix.
+#     e.g. if the config file name is  MSM-talaia_example.cfg the parameter should be 'talaia_example'
+
+#. /mnt/ebs/talaia_eupol/crawler/variables.sh
+. /etc/profile
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+data=`date +"%Y%m%d%H%M"`;
+
+pid=`pgrep -f " twitter .*MSM-$1.cfg"`
+echo "MSM-twtCrawler-restart.sh : $1 twitter crawlerra ITZALTZEN ... -- $data --"
+kill -9 $pid
+java -jar $DIR/MSM-1.2.jar twitter -c $DIR/MSM-$1.cfg -s db -p all 1>> $DIR/log/MSM-twitter.log 2>> $DIR/log/MSM-twitter.log &
+echo "MSM-twtCrawler-restart.sh : $1 twitter crawlerra BERRABIARAZITA ... -- $data --"
+
+
+
